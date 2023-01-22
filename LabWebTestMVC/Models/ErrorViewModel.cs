b8c81c4a732bed210fa1bc25e99adf0d2a49aff7
Evelyn <evelabohemia@gmail.com>
2023-01-22 16:52:05 -0300

    Subida primigenia

---
+++ LabWebTestMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabWebTestMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
